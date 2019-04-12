4d4c9ef129ce9a95a389e8dfceb94b7d671bef2f
肖文骏 <wb-xwj390510@antfin.com>
2019-04-12 18:09:54 +0800

    init classification

--- pages/classification/classification.js
+++ pages/classification/classification.js
@@ -5,62 +5,101 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    left_height: 0,
+    menuList: [{
+        'name': '全部',
+        'key': 'all'
+      },
+      {
+        'name': '热销',
+        'key': 'hot'
+      },
+      {
+        'name': '折扣',
+        'key': 'sale'
+      },
+    ],
+    dataList: [{
+        'id': 0,
+        'name': '99朵玫瑰',
+        'price': 700,
+      },
+      {
+        'id': 1,
+        'name': '19支红玫瑰礼盒',
+        'price': 238,
+      },
+      {
+        'id': 3,
+        'name': '33朵红玫瑰黑纱包装',
+        'price': 328,
+      },
+      {
+        'id': 4,
+        'name': '33朵love礼盒',
+        'price': 338,
+      },
+      {
+        'id': 5,
+        'name': '99朵白玫瑰',
+        'price': 658,
+      },
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,29 +6,29 @@ Page({
   data: {
     scroll_height: 0,
     indexList: [{
-        id: 0,
-        name: '99朵玫瑰',
-        price: 700,
+        'id': 0,
+        'name': '99朵玫瑰',
+        'price': 700,
       },
       {
-        id: 1,
-        name: '19支红玫瑰礼盒',
-        price: 238,
+        'id': 1,
+        'name': '19支红玫瑰礼盒',
+        'price': 238,
       },
       {
-        id: 3,
-        name: '33朵红玫瑰黑纱包装',
-        price: 328,
+        'id': 3,
+        'name': '33朵红玫瑰黑纱包装',
+        'price': 328,
       },
       {
-        id: 4,
-        name: '33朵love礼盒',
-        price: 338,
+        'id': 4,
+        'name': '33朵love礼盒',
+        'price': 338,
       },
       {
-        id: 5,
-        name: '99朵白玫瑰',
-        price: 658,
+        'id': 5,
+        'name': '99朵白玫瑰',
+        'price': 658,
       },
     ]
   },
@@ -37,11 +37,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    let windowHeight = wx.getSystemInfoSync().windowHeight // 屏幕的高度
-    let windowWidth = wx.getSystemInfoSync().windowWidth // 屏幕的宽度
+    let windowHeight = wx.getSystemInfoSync().windowHeight; // 屏幕的高度
+    let windowWidth = wx.getSystemInfoSync().windowWidth; // 屏幕的宽度
     this.setData({
       scroll_height: windowHeight * 750 / windowWidth
-    })
+    });
   },
 
   /**
